c633fd17b94a9831c427fa2e991693d0720c3e60
Ramamoorthy P <ramamoorthy163999@gmail.com>
2021-06-09 23:51:07 +0530

    added js-basics

---
+++ common.js
@@ -0,0 +1,24 @@
+function names() {
+    return `Ram Krish`
+}
+
+names();
+
+function numbers() {
+    return `1,2,3,4,5`
+}
+
+numbers();
+
+
+
+function boolean() {
+    let a = 20;
+    let b = 10;
+
+    return a > b;
+}
+
+boolean();
+
+module.exports = {names, numbers, boolean};
---
+++ user.js
@@ -0,0 +1,13 @@
+let func = require("./common");
+
+let num1 = func.names();
+let num2 = func.numbers();
+let num3 = func.boolean();
+
+// console.log(`names : ${num1}`);
+// console.log(`numbers : ${num2}`);
+// console.log(`boolean : ${num3}`);
+
+console.log(`the name is: ${num1}`);
+console.log(`the numbers is: ${num2}`);
+console.log(`the boolean is: ${num3}`);
